a49361a68f11a16bb24a0bf8b8afbf0c8436a8c1
AbhinavBahuguna2002 <91340952+AbhinavBahuguna2002@users.noreply.github.com>
2021-09-29 10:33:42 +0530

    Create Practical-04_Task2.cpp

---
+++ Practical-04/Practical-04_Task2.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+using namespace std;
+
+namespace first {
+  int add (int a, int b) { 
+    return (a+b);
+  } 
+}
+
+namespace second { 
+  float add (float a, float b) { 
+    return (a+b); 
+  }  
+} 
+
+using namespace first;
+using namespace second;
+
+int main () { 
+  cout << add(5,12); 
+  cout << "\n";
+  
+  cout << add(4.0, 8.1);
+  cout << "\n";
+   
+  cout << add(5.5, 2);
+  cout << "\n";
+  return 0;
+} 
