b8e359435ffaf24e7e11a46fc15de9919e652f8e
AbhinavBahuguna2002 <91340952+AbhinavBahuguna2002@users.noreply.github.com>
2021-10-02 22:52:32 +0530

    Add files via upload

---
+++ Practical-03_Task-4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main () {
+	string ch;
+	
+	cout << "Enter characters: ";
+	
+	getline(cin, ch);
+	cout << ch;
+	cout << "\n";
+	
+	return 0; 
+} 
