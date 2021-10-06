28761ec4113fb4fee296d0888df9cc4e02496324
AbhinavBahuguna2002 <91340952+AbhinavBahuguna2002@users.noreply.github.com>
2021-10-06 22:29:59 +0530

    Create Practical-06_Task-1.cpp

---
+++ Practical-06/Practical-06_Task-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+     int a= 55, b=15;
+     string c= "Hello", d= "World";
+     
+     cout << (a+b)<< endl;
+  
+     cout << (c+d) << endl;
+  
+     cout << (a+c) << endl;
+     
+     return 0;
+}
+ 
