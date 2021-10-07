0ac7d1fb72158560bda17e254fcd9acb31f3c16d
AbhinavBahuguna2002 <91340952+AbhinavBahuguna2002@users.noreply.github.com>
2021-10-07 10:15:18 +0530

    Create Practical-06_Task-5.cpp

---
+++ Practical-06/Practical-06_Task-5.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main () 
+{ 
+    string s1= "AAAA"; 
+    string s2= "ZZZZ"; 
+    
+    int x = s1.compare(s2); 
+    cout <<"Comparison of S1 & S2: " << x << endl;
+    
+    return 0; 
+} 
