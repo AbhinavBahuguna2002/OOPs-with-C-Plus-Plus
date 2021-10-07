b9e3097494b51ad5baa40c7fdc8a36af2ee00159
AbhinavBahuguna2002 <91340952+AbhinavBahuguna2002@users.noreply.github.com>
2021-10-07 23:13:21 +0530

    Create Practical-05_Task-1.cpp

---
+++ Practical-05/Practical-05_Task-1.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main()
+{   
+    short a = 5; 
+    cout << "Short Value: " << a << endl; 
+    
+    int b= 10; 
+    cout << "Int value: " << b << endl; 
+    
+    float c= 20.5f; 
+    cout << "Float value: " << c << endl;
+    
+    double d = 85.7643; 
+    cout << "Double value: " << d << endl; 
+    
+    long e= 566778;
+    cout << "Long value: " << e << endl; 
+    
+    long double f = 83458237489; 
+    cout << "Long Double: " << f << endl; 
+    
+    char ch= 'A';
+    cout << "Char value: " << ch << endl; 
+    
+    wchar_t ch2 = L'a';
+    cout << "Wide char value: " << ch2 << endl; 
+    
+    bool y = true;
+    cout << "Bool value: " << y << endl; 
+    
+    return 0;
+}
