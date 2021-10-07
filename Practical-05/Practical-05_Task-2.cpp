d36c552bf274b2c0fdbe3b954ff8c5340333272d
AbhinavBahuguna2002 <91340952+AbhinavBahuguna2002@users.noreply.github.com>
2021-10-07 23:26:39 +0530

    Create Practical-05_Task-2.cpp

---
+++ Practical-05/Practical-05_Task-2.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main()
+{   
+    short a = 5; 
+    cout << "sizeof(short): " << endl;
+    cout << sizeof(short) << endl; 
+    cout << sizeof(a) << endl <<endl; 
+    
+    int b= 10; 
+    cout << "sizeof(int): " << endl;
+    cout << sizeof(int) << endl; 
+    cout << sizeof(b) << endl <<endl; 
+    
+    float c= 20.5f; 
+    cout << "sizeof(float): " << endl;
+    cout << sizeof(float) << endl; 
+    cout << sizeof(c) << endl << endl; 
+    
+    double d = 85.7643; 
+    cout << "sizeof(double): " << endl;
+    cout << sizeof(double) << endl; 
+    cout << sizeof(d) << endl << endl; 
+    
+    long e= 566778;
+    cout << "sizeof(long): " << endl;
+    cout << sizeof(long) << endl; 
+    cout << sizeof(e) << endl << endl; 
+    
+    long double f = 83458237489; 
+    cout << "sizeof(long double): " << endl;
+    cout << sizeof(long double) << endl; 
+    cout << sizeof(f) << endl << endl; 
+    
+    char ch= 'A';
+    cout << "sizeof(char): " << endl;
+    cout << sizeof(char) << endl; 
+    cout << sizeof(ch) << endl << endl; 
+    
+    wchar_t ch2 = L'a';
+    cout << "sizeof(Wide char): " << endl;
+    cout << sizeof(wchar_t) << endl; 
+    cout << sizeof(ch2) << endl << endl; 
+    
+    bool y = true;
+    cout << "sizeof(bool): " << endl;
+    cout << sizeof(bool) << endl; 
+    cout << sizeof(y) << endl << endl; 
+    
+    return 0;
+}
