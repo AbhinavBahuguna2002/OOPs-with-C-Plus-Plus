b6c74a6097b56465916729052fd1b1476414d390
AbhinavBahuguna2002 <91340952+AbhinavBahuguna2002@users.noreply.github.com>
2021-10-13 09:46:41 +0530

    Update Practical-07_Task-1.cpp

--- Practical-07/Practical-07_Task-1.cpp
+++ Practical-07/Practical-07_Task-1.cpp
@@ -1,7 +1,30 @@
+/* This program prints Year, Month or Day as per user's choice using a switch manually (without ctime) */
+
 #include <iostream> 
-using namespace std;
+using namespace 
 
 int main ()
-{
+{	
+	int n;
+	cout << "What do you want to print?\n"; 
+	cout << "1. Year\n2. Month\n3. Day\n" << endl;
+	cout << "Enter your choice: "; 
+
+	cin >> n;
+
+	switch (n) 
+	{
+		case 1: cout << "2021\n";
+		break;
+		
+		case 2: cout << "October\n";
+		break;
+		
+		case 3: cout << "Wednesday\n"; 
+		break; 
+
+		default: cout << "Not applicable. Try again.\n";
+	} 
+
 	return 0;	
 }
