e7cf0b6ed3eee01af8f5019a27b2a2761135b823
AbhinavBahuguna2002 <91340952+AbhinavBahuguna2002@users.noreply.github.com>
2021-10-18 09:59:38 +0530

    Add files via upload

---
+++ Practical-08/Task3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std; 
+int main ()
+{
+	int a = 10; 
+
+	int *p; 
+	p = &a; 
+	
+	int **q; // double pointer 
+	cout << "Before change: " << *p << endl;
+	
+	q = &p;
+
+	**q = 30;
+	cout << "After change: " << a << endl;
+
+	return 0;
+}
+ 
+
