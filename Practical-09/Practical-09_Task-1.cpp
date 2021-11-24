52fcda0bbec8f63bd91503ceb734d5ad8b15ca9d
AbhinavBahuguna2002 <91340952+AbhinavBahuguna2002@users.noreply.github.com>
2021-11-24 23:46:09 +0530

    Update Practical-09_Task-1.cpp

--- Practical-09/Practical-09_Task-1.cpp
+++ Practical-09/Practical-09_Task-1.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class ABC {
+	
+	public: 
+	
+	int a, b;
+	
+	ABC (int x, int y) 
+	{
+		a = x;
+		b = y; 
+	}
+};
+
+int main () {
+	
+	ABC obj (10, 20); 
+	cout << obj.a << endl;
+	cout << obj.b << endl; 
+	
+	return 0; 
+} 
 
