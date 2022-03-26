07f6f0d1bbd5d1fb40820c524c2adfd9d4f74448
AbhinavBahuguna2002 <91340952+AbhinavBahuguna2002@users.noreply.github.com>
2022-03-26 14:25:50 +0530

    Add files via upload

---
+++ JAVA/Account.java
@@ -0,0 +1,41 @@
+import java.util.*; 
+class Account {
+	
+	String name; 
+	int acc;
+	float amount;
+	
+	void setDetails (String name, int acc, float amount) 
+	{
+ 		this.name = name;
+ 		this.acc = acc;  
+		this.amount = amount; 
+	
+	} 
+	
+	void show () 
+	{ 
+		System.out.println("Name: " + name + "\nAccount Number: " + acc + "\nAmount: " + amount); 
+	} 
+	public static void main (String args[]) 
+	{
+		Scanner sc= new Scanner (System.in);  
+		Account obj = new Account(); 
+		String name; 
+		int acc;
+		float amount;
+		System.out.print("Enter a name: ");
+		name = sc.nextLine(); 
+		System.out.print("\nEnter the Account Number:"); 
+		acc = sc.nextInt(); 
+		sc.nextLine(); 
+		System.out.print("\nEnter the Amount: ");
+		amount =sc.nextFloat(); 
+
+		System.out.println("\n");
+		obj.setDetails(name, acc, amount); 
+		obj.show(); 
+	
+	} 
+} 
+
