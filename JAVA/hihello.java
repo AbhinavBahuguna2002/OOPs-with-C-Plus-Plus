01e5515697d8b3a843c9de077bf20030eaf6f49f
AbhinavBahuguna2002 <91340952+AbhinavBahuguna2002@users.noreply.github.com>
2022-03-26 14:16:36 +0530

    Add files via upload

---
+++ JAVA/hihello.java
@@ -0,0 +1,17 @@
+/*
+Here the static method will be called automatically before executing 
+anything that is inside main method. 
+
+26-03-2022
+*/
+class Abc {
+	static {
+	System.out.println("hi..."); 
+	} 
+	
+	public static void main (String args[]) 
+	{ 
+	System.out.println("Hello"); 
+	} 
+} 
+
